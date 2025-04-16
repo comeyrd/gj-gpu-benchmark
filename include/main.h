d66e6514f5a65f5d70df3cb3bfd74c8ef9765820
Côme Eyraud <come.eyraud@polymtl.ca>
2025-04-16 14:58:38 -0400

    Upgrading command line possibilities

--- include/main.h
+++ include/main.h
@@ -10,24 +10,20 @@
 #include <stdio.h>
 #include <string.h>
 
-
-struct KernelStats{
+struct KernelStats {
     ExecutionStats e_stats;
     double mean_err;
     int repetitions;
     int size;
 };
 
-inline std::ostream& operator<<(std::ostream& os,KernelStats k_stat) {
-    os <<k_stat.e_stats<<" ~Error = "<<k_stat.mean_err<<" #Repetitions = "<<k_stat.repetitions<< " Matrix Size = "<<k_stat.size;
+typedef std::unordered_map<std::string, KernelStats> KStats_umap;
+
+inline std::ostream &operator<<(std::ostream &os, KernelStats k_stat) {
+    os <<  k_stat.e_stats << " ~Error = " << k_stat.mean_err << " #Repetitions = " << k_stat.repetitions << " Matrix Size = " << k_stat.size;
     return os;
 }
 
-
-void all_kernels();
-std::shared_ptr<IGaussJordan> find_kernel(std::string kernel);
-KernelStats do_kernel(std::shared_ptr<IGaussJordan> kernel_fn,int matrix_size, int repetitions);
+Kernel_umap find_kernel(std::vector<std::string> kernel_name_list);
+KStats_umap do_kernel(Kernel_umap kernels, int matrix_size, int repetitions);
 void list_kernels();
-
-
-
--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ int main(int argc, char **argv) {
     argparse::ArgumentParser program("gaussjordan");
     bool all;
     bool list;
-    int repetitions = 3;
-    int matrix_size = 50;
-    std::string kernel;
+    int repetitions = 5;
+    int matrix_size = 100;
+    std::vector<std::string> kernel;
 
     auto &group = program.add_mutually_exclusive_group(true);
 
     group.add_argument("-a", "--all").store_into(all);
-    group.add_argument("-k", "--kernel").store_into(kernel);
+    group.add_argument("-k", "--kernel").nargs(argparse::nargs_pattern::at_least_one).store_into(kernel); // Todo maybe list of strings to do multiple kernels ?
     group.add_argument("-l", "--list-kernels").store_into(list);
 
     program.add_argument("--repetitions", "-r").store_into(repetitions);
-    program.add_argument("--matrix-size", "-ms").store_into(matrix_size);
+    program.add_argument("--matrix-size", "-m").store_into(matrix_size);
 
     try {
         program.parse_args(argc, argv);
@@ -28,13 +28,20 @@ int main(int argc, char **argv) {
     }
 
     if (all) {
-       //To Implement
+        Kernel_umap Kmap = KernelsManager::instance()->getKernels();
+
+        KStats_umap ks = do_kernel(Kmap, matrix_size, repetitions);
+        for (const auto &[name, stats] : ks) {
+            std::cout << "Kernel " << name << " " << stats << std::endl;
+        }
     } else if (list) {
         list_kernels();
     } else {
-        std::shared_ptr<IGaussJordan> kernel_f = find_kernel(kernel);
-        KernelStats ks = do_kernel(kernel_f, matrix_size, repetitions);
-        std::cout << ks << std::endl;
+        Kernel_umap Kmap = find_kernel(kernel);
+        KStats_umap ks = do_kernel(Kmap, matrix_size, repetitions);
+        for (const auto &[name, stats] : ks) {
+            std::cout << "Kernel " << name << " " << stats << std::endl;
+        }
     }
 
     return 0;
@@ -47,35 +54,42 @@ void list_kernels() {
     }
 }
 
-std::shared_ptr<IGaussJordan> find_kernel(std::string kernel) {
-    Kernel_umap kernels = KernelsManager::instance()->getKernels();
-    try {
-        std::shared_ptr<IGaussJordan> k = kernels.at(kernel);
-        std::cout << "Running Kernel " << kernel << std::endl;
-        return k;
-    } catch (const std::exception error) {
-        std::cerr << "Kernel '" << kernel << "' not found" << std::endl;
-        exit(1);
+Kernel_umap find_kernel(std::vector<std::string> kernel_name_list) {
+    Kernel_umap kernels_map = KernelsManager::instance()->getKernels();
+    Kernel_umap filtered;
+
+    for (std::string kernel_name : kernel_name_list) {
+        try {
+            filtered[kernel_name] = kernels_map.at(kernel_name);
+        } catch (const std::exception error) {
+            std::cerr << "Kernel '" << kernel_name << "' not found" << std::endl;
+        }
     }
+
+    return filtered;
 }
 
-KernelStats do_kernel(std::shared_ptr<IGaussJordan> kernel_fn, int matrix_size, int repetitions) {
+KStats_umap do_kernel(Kernel_umap kernels, int matrix_size, int repetitions) {
     try {
         GJ_Utils::S_Matrix source = GJ_Utils::S_Matrix::Random_Invertible(matrix_size);
         GJ_Utils::GJ_Matrix gj(&source);
         GJ_Utils::S_Matrix out(matrix_size);
 
-        ExecutionStats e_stat;
-        double mean_err = 0;
-        for (int r = 0; r < repetitions; r++) {
-            e_stat += kernel_fn->inverse(&gj, &out);
-            mean_err += out.is_inverse(&source);
+        KStats_umap v_e_stat;
+        for (const auto &[name, k_func] : kernels) {
+            std::cout << "Running Kernel " << name << std::endl;
+            ExecutionStats e_stat;
+            double mean_err = 0;
+            for (int r = 0; r < repetitions; r++) {
+                e_stat += k_func->inverse(&gj, &out);
+                mean_err += out.is_inverse(&source);
+            }
+            e_stat = e_stat / repetitions;
+            mean_err = mean_err / repetitions;
+            KernelStats ks = {e_stat, mean_err, repetitions, matrix_size};
+            v_e_stat[name] = ks;
         }
-
-        e_stat = e_stat / repetitions;
-        mean_err = mean_err / repetitions;
-        KernelStats ks = {e_stat, mean_err, repetitions, matrix_size};
-        return ks;
+        return v_e_stat;
     } catch (const std::exception &error) {
         std::cerr << "Error running kernel" << std::endl;
         std::cerr << error.what() << std::endl;
